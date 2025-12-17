e9190429fb721e564cde2bb93fada59a8bbd8d0c
Craig Glendenning <craig@ziprecruiter.com>
2025-12-17 07:44:25 -0800

    add a flake.nix to the module so it can be used as a flake input

--- dr-dashboard/on-prem/build/flake.nix
+++ dr-dashboard/on-prem/build/flake.nix
@@ -1,21 +1,20 @@
 {
   description = "DR Dashboard On-Prem Kubernetes manifests";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    dr-dashboard.url = "git+ssh://git@github.com/OWNER/percona_operator?dir=dr-dashboard/on-prem/nix/modules/dr-dashboard";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, dr-dashboard }:
     let
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
     in
     {
       packages = forAllSystems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          drDashboard = import ../nix/modules/dr-dashboard { inherit pkgs; };
-        in
         {
-          default = drDashboard.mkManifests {
+          default = dr-dashboard.lib.${system}.mkManifests {
             imageTag = "latest";
             namespace = "default";
             ingressHost = "wookie.eko.dev.cookie.com";
